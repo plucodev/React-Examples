669c6cd2a7f92c67681132b6f6ac269f620c13f4
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-08-02 18:05:11 +0000

    updated calculator component

--- src/js/component/calculator.js
+++ src/js/component/calculator.js
@@ -1,37 +1,47 @@
-import React, { Component } from 'react';
-import './Calculator.css';
+import React, { Component } from "react";
+import "../../styles/calculator.scss";
 
 class Calculator extends Component {
-constructor() {
-super();
+	constructor() {
+		super();
 
-      this.state = {
-number1: 0,
-number2: 0,
-result: 0
-      };
-    }
+		this.state = {
+			number1: 0,
+			number2: 0,
+			result: 0
+		};
+	}
 
-render() {
-return (
-        <div className="Calculator">
-          <input
-            name="number1"
-            type="text"
-            value={this.state.number1}
-          />
-          {' + '}
-          <input
-            name="number2"
-            type="text"
-            value={this.state.number2}
-          />
+	handleOnChange(e) {
+		const {
+			target: { value, name }
+		} = e;
 
-          <p><button>=</button></p>
-          <p className="result">{this.state.result}</p>
-        </div>
-      );
-    }
-  }
+		this.setState({
+			[name]: Number(value)
+		});
+	}
+
+	handleResult(e) {
+		this.setState({
+			result: this.state.number1 + this.state.number2
+		});
+	}
+
+	render() {
+		return (
+			<div className="Calculator">
+				<input onChange={this.handleOnChange} name="number1" type="text" value={this.state.number1} />
+				{" + "}
+				<input onChange={this.handleOnChange} name="number2" type="text" value={this.state.number2} />
+
+				<p>
+					<button onClick={this.handleResult}>=</button>
+				</p>
+				<p className="result">{this.state.result}</p>
+			</div>
+		);
+	}
+}
 
 export default Calculator;
