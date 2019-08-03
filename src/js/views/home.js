0a180da399d0e96c3a2e38dd993804a8b7f2f68b
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-08-03 17:26:54 +0000

    Created Form Button in home

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Calculator from "../component/calculator";
+import Form from "../component/form";
 import { Link } from "react-router-dom";
 // import "../../styles/home.scss";
 
@@ -9,6 +10,9 @@ export class Home extends React.Component {
 			<React.Fragment>
 				<Link to="/calculator">
 					<button className="btn btn-success">Calculator</button>
+				</Link>
+				<Link to="/form">
+					<button className="btn btn-success">Form</button>
 				</Link>
 			</React.Fragment>
 		);
