131f858195242e33b39c45ec4cac71cef61dc993
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-08-06 02:10:38 +0000

    changed navbar style

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,13 +4,10 @@ import { Link } from "react-router-dom";
 export class Navbar extends React.Component {
 	render() {
 		return (
-			<nav className="navbar navbar-light bg-light mb-3">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Examples</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+			<nav className="navbar navbar-dark bg-dark  mb-3">
+				<div className="container">
+					<Link to="/">
+						<span className="navbar-brand mb-0 h1">React Examples</span>
 					</Link>
 				</div>
 			</nav>
