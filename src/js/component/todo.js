730eb75d44ac893bebe4e534d4530ec5eaf703f1
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-08-07 14:29:58 +0000

    Setting Todo

--- src/js/component/todo.js
+++ src/js/component/todo.js
@@ -1,3 +1,45 @@
 import React, { Component } from "react";
 
-export const Todo = () => <div>Todo</div>;
+export const Todo = () => (
+	<React.Fragment>
+		<div className="container">
+			<div className="row">
+				<div className="col-6">
+					<div className="input-group mb-3">
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Recipient's username"
+							aria-label="Recipient's username"
+							aria-describedby="button-addon2"
+						/>
+						<div className="input-group-append">
+							<button className="btn btn-success" type="button" id="button-addon2">
+								Add Todo
+							</button>
+						</div>
+					</div>
+				</div>
+				<div className="col-6">
+					<div className="input-group mb-3">
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Recipient's username"
+							aria-label="Recipient's username"
+							aria-describedby="button-addon2"
+						/>
+						<div className="input-group-append">
+							<button className="btn btn-success" type="button" id="button-addon2">
+								Add Todo
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="row">s</div>
+			<div className="row">s</div>
+		</div>
+	</React.Fragment>
+);
