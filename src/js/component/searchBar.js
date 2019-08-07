dd8eafbbf85c96cab6269b9d60e9e395fd8270ca
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-08-07 14:14:22 +0000

    Added searchBar component

---
+++ src/js/component/searchBar.js
@@ -0,0 +1,26 @@
+import React from "react";
+// import Calculator from "../component/calculator";
+// import Person from "../component/person";
+// import { Card } from "../component/card";
+import { Link } from "react-router-dom";
+// import "../../styles/home.scss";
+
+export default class SearchBar extends React.Component {
+	render() {
+		return (
+			<React.Fragment>
+				<div className="container">
+					<div className="row justify-content-center">
+						<div className="input-group mb-3">
+  <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+  <div className="input-group-append">
+    <button className="btn btn-info" type="button" id="button-addon2">Button</button>
+  </div>
+</div>
+					</div>
+
+				</div>
+			</React.Fragment>
+		);
+	}
+}
