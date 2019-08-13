d6d30caa25ee5537e6f6497261ec844daf8b92f7
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-08-13 19:50:24 +0000

    added queue component

---
+++ src/js/component/queue.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class Queue extends React.Component {
+	render() {
+		return (
+			<React.Fragment>
+				<div className="container">
+					<div className="row">Queue</div>
+				</div>
+			</React.Fragment>
+		);
+	}
+}
