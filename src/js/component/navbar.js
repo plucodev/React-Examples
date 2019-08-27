05d80c941d832415135ceffc6fc934deb4bb08e6
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-08-27 22:43:48 +0000

    added login button

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -9,6 +9,11 @@ export class Navbar extends React.Component {
 					<Link to="/">
 						<span className="navbar-brand mb-0 h1">React Examples</span>
 					</Link>
+					<form className="form-inline my-2 my-lg-0">
+						<button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+							Login
+						</button>
+					</form>
 				</div>
 			</nav>
 		);
