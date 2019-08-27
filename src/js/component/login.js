a2cb4648c372aae0c8b12e3877828c9cdf005181
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-08-27 22:49:14 +0000

    initialized login component

---
+++ src/js/component/login.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+export class Login extends React.Component {
+	render() {
+		return (
+			<Context.Consumer>
+				{({ store, actions }) => {
+					return (
+						<React.Fragment>
+            Login
+            </React.Fragment>
+					);
+				}}
+			</Context.Consumer>
+		);
+	}
+}
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,21 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export class Navbar extends React.Component {
 	render() {
 		return (
-			<nav className="navbar navbar-dark bg-dark  mb-3">
-				<div className="container">
-					<Link to="/">
-						<span className="navbar-brand mb-0 h1">React Examples</span>
-					</Link>
-					<form className="form-inline my-2 my-lg-0">
-						<button className="btn btn-outline-success my-2 my-sm-0" type="submit">
-							Login
-						</button>
-					</form>
-				</div>
-			</nav>
+			<Context.Consumer>
+				{({ store, actions }) => {
+					return (
+						<nav className="navbar navbar-dark bg-dark  mb-3">
+							<div className="container">
+								<Link to="/">
+									<span className="navbar-brand mb-0 h1">React Examples</span>
+								</Link>
+								<form className="form-inline my-2 my-lg-0">
+									<button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+										Login
+									</button>
+								</form>
+							</div>
+						</nav>
+					);
+				}}
+			</Context.Consumer>
 		);
 	}
 }
